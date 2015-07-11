97a6bf2d1b7aaf69ce5dfb76da95be5d0d4eddf7
TheBuzzSaw <TheBuzzSaw@Gmail.com>
2015-07-11 17:59:18 -0600

    Preparing templates.

--- source/hello.py
+++ source/hello.py
@@ -1,10 +1,30 @@
 from flask import Flask
+import psycopg2
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "STAR WARS"
+	return "Hello World!"
+
+@app.route("/card/<card>")
+def card(card):
+	try:
+		val = int(card)
+	except ValueError:
+		return "Error, invalid card number"
+	conn = psycopg2.connect("host=localhost dbname=swccg user=postgres password=guest222")
+	cur = conn.cursor()
+	cur.execute("select card_name from cards where id = (%s)", (card,))
+	r = cur.fetchone()
+	cur.close()
+	conn.close()
+
+	if r == None:
+		return "No card found with that ID"
+	else:
+		return "<h2>The card name is {}".format(r[0]) + "</h2><img src=\"/static/c" + card + ".gif\">"
 
 if __name__ == "__main__":
-    app.run()
-    
+	app.run(debug=True)
+	
