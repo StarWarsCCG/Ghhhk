a9b8fc1adbb4003b75844f40e4dab2b8ce6da69d
Koaxialkable <koaxialkable@gmail.com>
2015-07-12 16:04:52 -0600

    Added card title search API call

--- source/hello.py
+++ source/hello.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import psycopg2
+import json
 
 app = Flask(__name__)
 
@@ -24,6 +25,27 @@ def card(card):
 		return "No card found with that ID"
 	else:
 		return "<h2>The card name is {}".format(r[0]) + "</h2><img src=\"/static/images/c" + card + ".gif\">"
+
+@app.route("/api/cards/search_by_title", methods=['POST'])
+def search_by_title(search_string):	
+	# /api/cards/search_by_title
+	# POST data -> 'title_search'
+	# return a JSON doc of IDs and full card titles
+	search_string = '%' + search_string + '%'
+	conn = psycopg2.connect("host=localhost dbname=swccg user=postgres password=guest222")
+	cur = conn.cursor()
+	cur.execute("select id, card_name from cards where card_name ILIKE (%s)", (search_string,))
+	r = cur.fetchall()
+
+	data = {}
+	for row in r:
+		data[row[0]] = row[1]
+	output = json.dumps(data)
+
+	cur.close()
+	conn.close()
+	
+	return Response(output,  mimetype='application/json')
 
 if __name__ == "__main__":
 	app.run(debug=True)
