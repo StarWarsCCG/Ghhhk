e40db6282171067418d9735ece290eafc30de636
TheBuzzSaw <TheBuzzSaw@Gmail.com>
2015-07-12 20:52:45 -0600

    Added support for enter key.

--- source/static/deck_builder.js
+++ source/static/deck_builder.js
@@ -1,19 +1,29 @@
+function doSearch() {
+    $.getJSON(
+        "/api/cards/search?title=" + $("#card-title-search").val(),
+        null,
+        function(json) {
+            $('#card-search-results').empty();
+            
+            for (var key in json) {
+                if (json.hasOwnProperty(key)) {
+                    $('#card-search-results:last-child').append(
+                        '<tr><td>' + key + '</td><td>' + json[key] + '</td></tr>');
+                }
+            }
+        });
+}
+
 $(document).ready(function() {
     $("#card-title-search-button").click(function(e) {
         e.preventDefault();
-        $.getJSON(
-            "/api/cards/search?title=" + $("#card-title-search").val(),
-            null,
-            function(json) {
-                $('#card-search-results').empty();
-                
-                for (var key in json) {
-                    if (json.hasOwnProperty(key)) {
-                        $('#card-search-results:last-child').append(
-                            '<tr><td>' + key + '</td><td>' + json[key] + '</td></tr>');
-                    }
-                }
-            });
+        doSearch();
+    });
+    
+    $("#card-title-search").keydown(function(e) {
+        if (e.keyCode == 13) {
+            doSearch();
+        }
     });
 });
 
